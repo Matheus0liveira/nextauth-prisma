d89b70eaf6595b9d6a1ca8e4a7107f3893d1853e
matheus0liveira <matheusfilho98.ms@gmailcom>
2023-01-24 22:35:02 -0300

    chore: add prisma setup

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '../../../lib/prismadb';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '@/utils/constants';
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -12,4 +12,6 @@ export default NextAuth({
       clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+};
+
+export default NextAuth(authOptions);
